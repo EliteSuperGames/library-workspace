b0626d6ce41715adf3bac248c71befd879ec1a6b
david <davidgriffin.2@gmail.com>
2025-01-23 18:15:54 -0500

    add exports

--- projects/shared-library/src/public-api.ts
+++ projects/shared-library/src/public-api.ts
@@ -4,3 +4,8 @@
 
 export * from './lib/shared-library.service';
 export * from './lib/shared-library.component';
+export * from './lib/models/address';
+// export * from './lib/components';
+// export * from './lib/directives';
+// export * from './lib/models';
+// export * from './lib/services';
